2e9310f36c9588342aea3ce8be2be73a9c13568e
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-10-08 15:44:28 +0800

    Add files via upload

--- docker.sh
+++ docker.sh
@@ -35,14 +35,14 @@ if [[ -z "${Ubuntu}" ]] && [[ -z "${Debian}" ]]; then
 	echo
 	exit 1
 fi
-apt install -y sudo
-apt install -y dpkg
-if [[ `dpkg -l | grep -c "docker"` -ge '1' ]]; then
+apt install -y sudo curl wget
+if [[ `docker --version | grep -c "version"` -ge '1' ]]; then
 	echo
 	TIME y "检测到docker存在，是否重新安装?"
 	echo
 	TIME g "重新安装会把您现有的所以容器及镜像全部删除"
 	echo
+	while :; do
 	read -p " [输入[ N/n ]退出安装，输入[ Y/y ]回车继续]： " ANDK
 	case $ANDK in
 		[Yy])
@@ -61,22 +61,46 @@ if [[ `dpkg -l | grep -c "docker"` -ge '1' ]]; then
 			sudo rm -rf /etc/docker
 			sudo rm -rf /lib/systemd/system/{docker.service,docker.socket}
 			rm /var/lib/dpkg/info/$nomdupaquet* -f
+		break
 		;;
 		[Nn])
-			TIME r "退出安装程序!"
-			sleep 2
+			echo
+			TIME r "您选择了退出程序!"
+			echo
+			sleep 3
 			exit 1
+		break
+    		;;
+    		*)
+			echo
+			TIME b "提示：请输入正确的选择!"
+			echo
 		;;
 	esac
+	done
 fi
+echo
+TIME y "正在安装docker，请耐心等候..."
+echo
 sudo -E apt-get -qq update
 sudo -E apt-get -qq install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 if [[ "${Ubuntu}" == "ubuntu" ]]; then
 	curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+	if [[ $? -ne 0 ]];then
+		curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+	fi
 else
 	curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
+	if [[ $? -ne 0 ]];then
+		curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add -
+	fi
 fi
 sudo apt-key fingerprint 0EBFCD88
+if [[ `sudo apt-key fingerprint 0EBFCD88 | grep -c "0EBF CD88"` = '0' ]]; then
+	TIME r "密匙验证出错"
+	sleep 2
+	exit 1
+fi
 if [[ "${Ubuntu}" == "ubuntu" ]]; then
 	sudo add-apt-repository -y "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
 else
@@ -90,20 +114,22 @@ if [[ `grep -c "dockerd -H fd://" build.log` -ge '1' ]]; then
 	sed -i 's#ExecStart=/usr/bin/dockerd -H fd://#ExecStart=/usr/bin/dockerd#g' /lib/systemd/system/docker.service
 	sudo systemctl daemon-reload
 fi
-sudo systemctl restart docker
 sudo rm -fr build.log
 sudo rm -fr docker.sh
-sleep 10
-if [[ `dpkg -l | grep -c "docker"` -eq '0' ]]; then
+if [[ `docker --version | grep -c "version"` = '0' ]]; then
 	TIME y "docker安装失败"
 	sleep 2
 	exit 1
 else
 	TIME y ""
+	TIME g "docker安装成功，正在重启docker，请稍后..."
+	sudo systemctl restart docker
+	sleep 12
+	TIME y ""
 	TIME g "测试docker拉取镜像是否成功"
 	TIME y ""
-	sudo docker run hello-world
-	if [[ `docker ps -a | grep -c "hello-world"` -ge '1' ]]; then
+	sudo docker run hello-world |tee build.log
+	if [[ `docker ps -a | grep -c "hello-world"` -ge '1' ]] && [[ `grep -c "docs.docker" build.log` -ge '1' ]]; then
 		echo
 		TIME g "测试镜像拉取成功，正在删除测试镜像..."
 		echo
@@ -115,10 +141,12 @@ else
 		echo
 	else
 		echo
-		TIME y "docker安装成功虽然安装成功但是拉取镜像失败，这个原因很多是因为以前的docker没御载完全造成的"
+		TIME y "docker安装成功虽然安装成功但是拉取镜像失败，这个原因很多是因为以前的docker没御载完全造成的，或者容器网络问题"
 		echo
 		TIME y "重启服务器后，用 sudo docker run hello-world 命令测试吧，能拉取成功就成了"
 		echo
+		sleep 2
+		exit 1
 	fi
 fi
 exit 0
--- ql.sh
+++ ql.sh
@@ -27,23 +27,92 @@ TIME() {
 	exit 1
 }
 
+if [[ "$USER" == "root" ]]; then
+	clear
+	echo
+	echo
+	echo
+	TIME z "脚本适用于（ubuntu、debian、openwrt）"
+	TIME z "一键安装青龙，包括（docker、任务、依赖安装，一条龙服务）"
+	TIME z "自动检测docker，有则跳过，无则执行安装，如果是openwrt则请自行安装docker和挂载好硬盘"
+	TIME z "如果您以前安装有青龙的话，则自动删除您的青龙容器和镜像，全部推倒重新安装"
+	TIME z "如果您有需要备份的青龙文件，请先备份好文件再来安装，免得损失"
+	TIME z "如果有条件的话，最好使用翻墙网络来安装，要不然安装依赖的时候你会急死的"
+	TIME g "如要不能接受的话，请选择 3 回车退出程序!"
+	echo
+	echo
+	echo
+	TIME y " 如要继续的话，请选择容器的网络类型!（输入 1、2或3 编码）"
+	echo
+	TIME l " 1. bridge [默认类型]"
+	echo
+	TIME l " 2. host [一般为openwrt旁路由才选择的]"
+	echo
+	TIME l " 3. 退出安装程序!"
+	echo
+	while :; do
+	read -p " [输入您选择的编码]： " SCQL
+	case $SCQL in
+		1)
+			QL_PORT="5700"
+			QING_PORT="YES"
+			NETWORK="-p ${QL_PORT}:5700"
+		break
+		;;
+		2)
+			NETWORK="--net host"
+			QL_PORT="5700"
+		break
+		;;
+		3)
+			echo
+			TIME r "您选择了退出程序!"
+			echo
+			sleep 3
+			exit 1
+		break
+    		;;
+    		*)
+			echo
+			TIME b "提示：请输入正确的选择!"
+			echo
+		;;
+	esac
+	done
+fi
+echo
+echo
+[[ "${QING_PORT}" == "YES" ]] && {
+	TIME g "请设置端口，默认为端口[5700]，不需要设置的话，直接回车跳过"
+	read -p " 请输入端口：" QL_PORT
+	QL_PORT=${QL_PORT:-"5700"}
+	TIME y "您端口为：${QL_PORT}"
+	NETWORK="-p ${QL_PORT}:5700"
+}
+echo
+echo
+TIME g "正在安装宿主机所需要的依赖，请稍后..."
+echo
 if [[ -z "$(ls -A "/etc/openwrt_release" 2>/dev/null)" ]]; then
-	sudo -E apt-get -qq update
-	apt install -y sudo curl dpkg wget
+	QL_PATH="/opt"
+	apt update
+	apt install -y sudo curl wget
+else
+	QL_PATH="$PWD"
 fi
 
 if [[ -n "$(ls -A "/etc/openwrt_release" 2>/dev/null)" ]]; then
-	if [[ `docker version | grep -c "version"` -eq '0' ]]; then
+	if [[ `docker --version | grep -c "version"` -eq '0' ]]; then
 		echo
-		TIME y "没检测到docker，请先安装docker"
+		TIME y "没检测到docker，openwrt请自行安装docker和挂载好硬盘"
 		echo
 		sleep 3
 		exit 1
 	fi
 else
-	if [[ `docker version | grep -c "version"` -eq '0' ]]; then
+	if [[ `docker --version | grep -c "version"` -eq '0' ]]; then
 		echo
-		TIME y "没检测到docker，正在安装docker，请稍后..."
+		TIME y "没发现有docker，正在安装docker，请稍后..."
 		echo
 		wget -O docker.sh https://ghproxy.com/https://raw.githubusercontent.com/281677160/ql/main/docker.sh && bash docker.sh
 		if [[ $? -ne 0 ]];then
@@ -53,15 +122,15 @@ else
 	fi
 fi
 if [[ -n "$(ls -A "/etc/openwrt_release" 2>/dev/null)" ]]; then
-	if [[ `docker version | grep -c "version"` -eq '0' ]]; then
+	if [[ `docker --version | grep -c "version"` -eq '0' ]]; then
 		echo
-		TIME y "没检测到docker，请先安装docker"
+		TIME y "没检测到docker，openwrt请自行安装docker和挂载好硬盘"
 		echo
 		sleep 3
 		exit 1
 	fi
 else
-	if [[ `docker version | grep -c "version"` -eq '0' ]]; then
+	if [[ `docker --version | grep -c "version"` -eq '0' ]]; then
 		echo
 		TIME y "没检测到docker，请先安装docker"
 		echo
@@ -69,89 +138,64 @@ else
 		exit 1
 	fi
 fi
-if [[ `docker ps -a | grep -c "whyour"` -ge '1' ]]; then
+if [[ `docker ps | grep -c "whyour"` -ge '1' ]]; then
 	echo
 	TIME g "检测到已有青龙面板，需要删除面板才能继续..."
 	echo
-	TIME y "如果要继续的话，请注意备份你原来的配置文件!"
+	TIME y "正在删除旧的青龙容器和镜像，请稍后..."
 	echo
-	read -p " [输入[ N/n ]退出安装，输入[ Y/y ]回车继续]： " SCQL
-	case $SCQL in
-		[Yy])
-			echo
-			TIME y "正在停止您以前的青龙然后删除，请稍后...!"
-			echo
-			docker=$(docker ps|grep whyour) && dockerid=$(awk '{print $(1)}' <<<${docker})
-			images=$(docker images|grep whyour) && imagesid=$(awk '{print $(3)}' <<<${images})
-			docker stop -t=5 "${dockerid}"
-			docker rm "${dockerid}"
-			docker rmi "${imagesid}"
-		;;
-		[Nn])
-			TIME r "退出安装程序!"
-			sleep 2
-			exit 1
-		;;
-	esac
+	docker=$(docker ps|grep whyour) && dockerid=$(awk '{print $(1)}' <<<${docker})
+	images=$(docker images|grep whyour) && imagesid=$(awk '{print $(3)}' <<<${images})
+	docker stop -t=5 "${dockerid}"
+	docker rm "${dockerid}"
+	docker rmi "${imagesid}"
 fi
-
 rm -rf /opt/ql
 rm -rf /root/ql
-sleep 3
 echo
-
-if [[ -n "$(ls -A "/etc/openwrt_release" 2>/dev/null)" ]]; then
 echo
 TIME g "正在安装青龙面板，请稍后..."
 echo
 docker run -dit \
-  -v $PWD/ql/config:/ql/config \
-  -v $PWD/ql/log:/ql/log \
-  -v $PWD/ql/db:/ql/db \
-  -v $PWD/ql/scripts:/ql/scripts \
-  -v $PWD/ql/jbot:/ql/jbot \
-  -v $PWD/ql/raw:/ql/raw \
-  -v $PWD/ql/repo:/ql/repo \
-  --net host \
+  -v $QL_PATH/ql/config:/ql/config \
+  -v $QL_PATH/ql/log:/ql/log \
+  -v $QL_PATH/ql/db:/ql/db \
+  -v $QL_PATH/ql/scripts:/ql/scripts \
+  -v $QL_PATH/ql/jbot:/ql/jbot \
+  -v $QL_PATH/ql/raw:/ql/raw \
+  -v $QL_PATH/ql/repo:/ql/repo \
+  ${NETWORK} \
   --name qinglong \
   --hostname qinglong \
   --restart always \
   whyour/qinglong:latest
-else
-TIME g "正在安装青龙面板，请稍后..."
-echo
-docker run -dit \
-   -v /opt/ql/config:/ql/config \
-   -v /opt/ql/log:/ql/log \
-   -v /opt/ql/db:/ql/db \
-   -v /opt/ql/scripts:/ql/scripts \
-   -v /opt/ql/jbot:/ql/jbot \
-   -v /opt/ql/raw:/ql/raw \
-   -v /opt/ql/repo:/ql/repo \
-   -p 5700:5700 \
-   --name qinglong \
-   --hostname qinglong \
-   --restart always \
-   whyour/qinglong:latest
-fi
 
 if [[ `docker ps -a | grep -c "whyour"` -ge '1' ]]; then
 	docker restart qinglong
 	sleep 13
+	clear
+	echo
+	echo
 	echo
 	TIME z "青龙面板安装完成"
 	echo
-	TIME g "请使用 IP:5700 在浏览器登录控制面板，然后在环境变量里添加好WSKEY或者PT_KEY"
+	TIME g "请使用 IP:${QL_PORT} 在浏览器登录控制面板，然后在环境变量里添加好WSKEY或者PT_KEY"
 	echo
-	TIME y "您也可以不添加WSKEY或者PT_KEY，但是一定要登录管理面"
+	TIME y "您也可以不添加WSKEY或者PT_KEY，但是一定要登录控制面板"
 	echo
-	TIME g "重要提示：重要，一定要登录管理面板之后再执行下一步操作！！！"
+	TIME g "登录页面，点击[开始安装]，设置好[用户名]跟[密码],然后点击[提交]，[通知方式]跳过，以后再设置，然后点击[去登录]，完成!"
 	echo
-	TIME y "输入 N 回车退出程序，或者进入过管理页面后输入 Y 回车继续安装脚本"
-	echo
+	while :; do
 	read -p " [ N/n ]退出程序，[ Y/y ]回车继续安装脚本： " MENU
+	if [[ `docker exec -it qinglong bash -c "cat /ql/config/auth.json" | grep -c "\"token\""` -ge '1' ]]; then
+		S="Yy"
+	else
+		echo
+		TIME r "提示：一定要登录管理面板之后再执行下一步操作,或者您输入[N/n]按回车退出!"
+		echo
+	fi
 	case $MENU in
-		[Yy])
+		[${S}])
 			echo
 			TIME y "开始安装脚本，请耐心等待..."
 			echo
@@ -160,28 +204,39 @@ if [[ `docker ps -a | grep -c "whyour"` -ge '1' ]]; then
 				docker exec -it qinglong bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/281677160/ql@main/feverrun.sh)"
 			fi
 			rm -fr ql.sh
+		break
 		;;
 		[Nn])
+			echo
 			TIME r "退出安装程序!"
+			echo
 			sleep 2
 			exit 1
+		break
+    		;;
+    		*)
+			TIME r ""
 		;;
 	esac
+	done
 else
+	echo
+	echo
 	TIME y "青龙面板安装失败！"
+	echo
 	sleep 2
 	exit 1
 fi
 echo
 echo
-if [[ `docker ps -a | grep -c "whyour"` -ge '1' ]]; then
+if [[ `docker ps | grep -c "whyour"` -ge '1' ]]; then
 	TIME l "安装依赖，依赖必须安装，要不然脚本不运行"
 	echo
 	TIME y "但是用国内的网络安装比较慢，请尽量使用翻墙网络"
 	echo
 	TIME g "没翻墙条件的话，安装依赖太慢就换时间安装，我测试过不同时段有不同效果"
 	echo
-	TIME y "依赖安装时看到显示ERR!错误提示的，不用管，只要依赖能从头到尾的下载运行完毕就好了"
+	TIME y "依赖安装时看到显示ERR!错误提示不用管，只要依赖能从头到尾的下载运行完毕就好了"
 	echo
 	TIME g "如果安装太慢，而想换时间安装的话，按键盘的 Ctrl+C 退出就行了，到时候可以使用我的一键独立安装依赖脚本来安装"
 	echo
